5f903b569394d30fffb196f75c86e72e6327211c
tm023756 <teymur.mammadov@ozu.edu.tr>
2024-11-26 23:53:04 +0300

    Added Categories ViewModel and added Category values

--- app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/ui/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/ui/categories/CategoriesViewModel.kt
@@ -1,4 +1,22 @@
 package com.faridjeyhunhuseyinteymur.newsly.ui.categories
 
-class CategoriesViewModel {
+class CategoriesViewModel : ViewModel() {
+    private val _categories = MutableLiveData<List<String>>()
+    val categories: LiveData<List<String>> = _categories
+
+    init {
+        loadCategories()
+    }
+
+    private fun loadCategories() {
+        _categories.value = listOf(
+            "Business",
+            "Entertainment",
+            "General",
+            "Health",
+            "Science",
+            "Sports",
+            "Technology"
+        )
+    }
 }
