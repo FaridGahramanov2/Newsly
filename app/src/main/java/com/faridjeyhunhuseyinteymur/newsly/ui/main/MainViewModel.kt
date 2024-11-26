dd8f9183bc75ffcd7b84670e6f001a943f32e956
tm023756 <teymur.mammadov@ozu.edu.tr>
2024-11-26 23:46:12 +0300

    Changed the MainViewModel according to our project

--- app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/ui/main/MainViewModel.kt
+++ app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/ui/main/MainViewModel.kt
@@ -1,4 +1,12 @@
 package com.faridjeyhunhuseyinteymur.newsly.ui.main
 
-class MainViewModel {
+class MainViewModel : ViewModel() {
+    private val repository = NewsRepository()
+
+    private val _selectedCategory = MutableLiveData<String>()
+    val selectedCategory: LiveData<String> = _selectedCategory
+
+    fun setCategory(category: String) {
+        _selectedCategory.value = category
+    }
 }
