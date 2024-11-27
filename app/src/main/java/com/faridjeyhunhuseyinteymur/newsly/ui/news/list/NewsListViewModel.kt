00fd33bdafe82161bc558bc594a4e55d0082ad37
Huseyn Rasulzade <huseynrasulzade@Huseyns-Laptop-4.local>
2024-11-27 14:19:22 +0300

    Added NewsListViewModel for our mock data and described what will be done for part 2

--- app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/ui/news/list/NewsListViewModel.kt
+++ app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/ui/news/list/NewsListViewModel.kt
@@ -1,4 +1,27 @@
 package com.faridjeyhunhuseyinteymur.newsly.ui.news.list
 
-class NewsListViewModel {
+import com.faridjeyhunhuseyinteymur.newsly.data.repository.NewsRepository
+
+class NewsListViewModel : ViewModel() {
+    private val repository = NewsRepository()
+
+    private val _articles = MutableLiveData<List<Article>>()
+    val articles: LiveData<List<Article>> = _articles
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        loadMockNews()
+    }
+
+    private fun loadMockNews() {
+        _isLoading.value = true
+        _articles.value = repository.getMockNews()
+        _isLoading.value = false
+    }
+
+    // TODO: Will be implemented in Part 2
+    // suspend fun loadNews()
+    // suspend fun loadNewsByCategory(category: String)
 }
