2438beca2762a392bd2c895499ee4ae1c2573bbe
Huseyn Rasulzade <huseynrasulzade@Huseyns-Laptop-4.local>
2024-11-27 14:59:18 +0300

    Implemented NewsViewHolder and added Source data class

---
+++ app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/data/model/Source.kt
@@ -0,0 +1,8 @@
+package com.faridjeyhunhuseyinteymur.newsly.data.model
+
+data class Source(
+    val id: String,
+    val name: String
+) {
+    // TODO: Will be used in Part 2 for API integration
+}
--- app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/ui/news/list/NewsViewHolder.kt
+++ app/src/main/java/com/faridjeyhunhuseyinteymur/newsly/ui/news/list/NewsViewHolder.kt
@@ -1,4 +1,15 @@
 package com.faridjeyhunhuseyinteymur.newsly.ui.news.list
 
-class NewsViewHolder {
+class NewsViewHolder(
+    private val binding: ItemNewsBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(article: Article) {
+        binding.apply {
+            titleTextView.text = article.title
+            descriptionTextView.text = article.description
+            newsImageView.setImageResource(article.urlToImage)
+            categoryChip.text = article.category
+        }
+    }
 }
